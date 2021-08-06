efc19b4ca49eee4ab7797480f17ebff58877b2cf
Windymelt <windymelt@3qe.us>
2021-08-06 20:36:55 +0900

    typing

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    return events.APIGatewayProxyResponse{}, nil
+type MyEvent struct {
+	ScreenName string `json:"screen_name"`
+	PNGBase64  string `json:"pngbase64"`
+}
+
+type MyResponse struct {
+	URI string `json:"uri:"`
+}
+
+func handler(request MyEvent) (MyResponse, error) {
+	// extract image file from event
+	// save image into temporary file
+	// call primitive
+	// load result image
+	// upload file into S3
+	// return image URI
+	return MyResponse{URI: ""}, nil
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
